3e32b8ef8265c7ed158d69d22732ec9348b63306
Letícia <111643803+leiticiasf@users.noreply.github.com>
2024-02-27 12:36:29 -0300

    Update App.jsx
    correção elemento errado "<nbsp/>"

--- navegacaoPag/vite-project/src/App.jsx
+++ navegacaoPag/vite-project/src/App.jsx
@@ -14,7 +14,7 @@ function App() {
       {}
         <Route path="/" element={<Home />} />        
        {}
-        <Route path="/sobre" element={<Sobre />} /><npbs/>
+        <Route path="/sobre" element={<Sobre />} /> 
         {}
         <Route path="/materias" element={<Materias />}/>
       </Routes>
